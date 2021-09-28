56a78f8124d16a8b5f833ebfe171bac05743c500
vincent <vincent.cy@foxmail.com>
2021-09-28 08:15:11 +0800

    mmodify

--- server/config.ts
+++ server/config.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 
 const isDev: boolean = process.env.NODE_ENV !== 'production';
 
+console.log(' process.env.NODE_ENV',  process.env.NODE_ENV);
+
 const envFile = `.env.${isDev ? 'dev' : 'prod'}`;
 
 dotenv.config({ path: envFile });
@@ -15,7 +17,7 @@ export interface Config {
 const config: Config = {
     port: +(process.env.PORT || 3000),
     isDev: isDev,
-    api_host: process.env.API_HOST || '/'
+    api_host: process.env.API_HOST || ''
 };
 
 export { config };
