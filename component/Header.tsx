44bca218e9d82ce04e64a6f140d87b415a6927c4
vincent <vincent.cy@foxmail.com>
2021-10-03 09:35:05 +0800

    'cicd'

--- component/Header.tsx
+++ component/Header.tsx
@@ -11,7 +11,7 @@ const Header = (props : any) => {
         <meta name="keywords" content={keyword} />
         <meta name='description' content={description} />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        {/*<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />*/}
         <link type="text/css" rel="styleSheet" href="//at.alicdn.com/t/font_1987433_wl53fgt2ay.css" />
         <meta name="shenma-site-verification" content="5397f6d950bbbad584d6f83dc807f7c1_1597487844"></meta>
     </Head>;
