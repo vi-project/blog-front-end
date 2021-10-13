1ca3e9ab8590e20ce91a0c38235f05d8533d8f76
vincent <vincent.cy@foxmail.com>
2021-10-13 17:58:31 +0800

    feat: add sitemap and modify rebots

---
+++ pages/sitemap.xml/index.ts
@@ -0,0 +1,34 @@
+import { getServerSideSitemap } from 'next-sitemap';
+import { GetServerSideProps } from 'next';
+import request from '../../utils/request';
+import { Article } from '../../@types';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    // const urls = await fetch('https//example.com/api')
+    const data: any = await request.get(`/article`, { size: 1000 });
+
+    const urls = data.list.map((u: Article) => ({
+        loc: `https://wekic.com/article/${u.id}`,
+        lastmod: new Date().toISOString(),
+        changefreq: "monthly"
+        // priority
+    }));
+
+    urls.push({
+        loc: 'https://wekic.com',
+        lastmod: new Date().toISOString(),
+        // changefreq
+        // priority
+    });
+    urls.push({
+        loc: 'https://wekic.com/article',
+        lastmod: new Date().toISOString(),
+        // changefreq
+        // priority
+    });
+
+    return getServerSideSitemap(ctx, urls);
+};
+
+// Default export to prevent next.js errors
+export default () => null;
