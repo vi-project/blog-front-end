c4aff9e2cf06762175b0825ff21339160a49e181
vincent <vincent.chen@homepartners.tech>
2023-02-15 08:08:29 +0800

    fix(): 去除字体库

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,18 +1,11 @@
 import presetAttributify from "@unocss/preset-attributify";
 import presetUno from "@unocss/preset-uno";
-import presetWebFonts from "@unocss/preset-web-fonts";
 import {defineConfig, presetIcons} from "unocss";
 
 export default defineConfig({
     presets: [
         presetUno(),
         presetAttributify(),
-        presetWebFonts({
-            fonts: {
-                sans: 'Inter:400,600,800',
-                mono: 'DM Mono',
-            },
-        }),
         presetIcons({
             collections: {
                 tabler: () => import('@iconify-json/tabler').then((i) => i.icons as any)
