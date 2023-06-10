393b4947f3483103a5a9432e276145181fdb7d3d
vincent <vincent.chen@homepartners.tech>
2023-06-11 07:38:26 +0800

    feat(): 简历背景

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -1,16 +1,16 @@
 import React, { useRef } from "react";
 import request from '../../utils/request';
-import { decodeMarkDown } from "../../utils/parse";
+import {genMarkdown} from "../../utils/parse";
 
 const ResumeView: React.FunctionComponent<any> = (props) => {
     const contentRef = useRef<HTMLDivElement>(null);
-    const ht_ = decodeMarkDown(props.content || "");
+    const ht_ = props.content || "";
     return (
         <div className="prose m-auto">
             <article>
                 <div ref={contentRef}
                     className="post-content"
-                    style={{ paddingTop: "2rem" }}
+                    style={{ paddingTop: "2rem", backgroundColor: 'white' }}
                     dangerouslySetInnerHTML={{ __html: ht_ }}
                 />
             </article>
@@ -18,16 +18,15 @@ const ResumeView: React.FunctionComponent<any> = (props) => {
     );
 };
 
-
 export async function getServerSideProps(): Promise<any> {
     try {
         const { data } = await request.get(`/user/resume`, {});
+        const md = await genMarkdown();
+        data.content = md.render(data.content);
         return { props: data };
     } catch (e) {
         return { props: { error: 'article not exists.' } };
     }
-
 }
-
 
 export default ResumeView;
