1e32c7f16ef26883a311726996dd25256233b69a
vincent <vincent.cy@foxmail.com>
2024-11-24 09:52:33 +0800

    ”omom”

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -3,7 +3,11 @@ export default function Footer() {
     return <footer className="w-full bottom-0">
         <div className="mt-10 mb-6 prose m-auto opacity-50 flex text-center">
             <span className="text-sm align-middle">
-                © Vincent Chen
+                <a style={{  textDecoration: 'none' }}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://beian.miit.gov.cn"
+                >湘ICP备20014327号-1</a>
             </span>
         </div>
     </footer>;
